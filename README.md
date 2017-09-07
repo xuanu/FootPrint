1fa33012c7b5bdf8dfa93b1cd537b9d62bc47f96
zeffect <625417632@qq.com>
2017-09-07 17:21:56 +0800

    图

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # footprint
 足迹，地图打点应用
+> 简单的想记录一下自己每天的足迹，顺便学习一下Kotlin的语法。
+
+#### 问题
+1. GPS会消耗大量电量,所以没有采用GPS定位
+2. 每分钟记录一次，会产生大量数据，存入/sdcard/footprint/db/loaction.db数据库
+***
+#### 截图
+![image](https://github.com/xuanu/footprint/raw/master/screenshots/Screenshot_20170907-171616.jpg)
+![image](https://github.com/xuanu/footprint/raw/master/screenshots/Screenshot_20170907-171622.jpg)
+
--- app/src/main/java/apk/cn/zeffect/footprint/service/LocationService.kt
+++ app/src/main/java/apk/cn/zeffect/footprint/service/LocationService.kt
@@ -94,7 +94,7 @@ class LocationService : Service() {
         option.setCoorType("bd09ll")//可选，默认gcj02，设置返回的定位结果坐标系1
         option.setScanSpan(INTERVAL_TIME)//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true)//可选，设置是否需要地址信息，默认不需要
-        option.isOpenGps = true//可选，默认false,设置是否使用gps
+        option.isOpenGps = false//可选，默认false,设置是否使用gps
         option.isLocationNotify = false//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
         option.setIsNeedLocationDescribe(true)//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
         option.setIsNeedLocationPoiList(false)//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
